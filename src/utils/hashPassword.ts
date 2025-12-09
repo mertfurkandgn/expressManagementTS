8f5d90bdcd823fc99bacb1484f5dcd0bb4824067
mertfurkandgn <mertfurkandgn1912@gmail.com>
2025-12-09 23:22:52 +0300

    password hash and compare utils add

--- src/utils/hashPassword.ts
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export async function hashPassword(password: string, rounds = 10) {
-  if (!password?.trim()) {
-    throw new Error("Password is required");
-  }
-
-  return bcrypt.hash(password, rounds);
-}
---
+++ src/utils/password.ts
@@ -0,0 +1,17 @@
+import bcrypt from "bcryptjs";
+
+export async function hashPassword(password: string, rounds = 10) {
+  if (!password?.trim()) {
+    throw new Error("Password is required");
+  }
+
+  return await bcrypt.hash(password, rounds);
+}
+
+export async function comparePassword(password: string, hashedPassword: string) { 
+  if (!password?.trim() || !hashedPassword?.trim()) {
+    throw new Error("Undefined password.");
+  }
+
+  return await bcrypt.compare(password, hashedPassword);
+}
